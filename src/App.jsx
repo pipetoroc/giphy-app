927a52b901ce87563049d556f45ed89358fcf8ad
Felipe Toro C <ftorocastilla@gmail.com>
2023-04-29 13:06:52 -0300

    feat: use useEffect to render gifs with trending gifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App () {
+
+  const [gifs, setGifs] = useState([])
+
+  const  API_URL = 'https://api.giphy.com/v1/gifs/trending?api_key=pJqYviFUDNfFcku0l5QR8kqFgRDjny45&limit=25&rating=g'
+  const API_KEY = 'pJqYviFUDNfFcku0l5QR8kqFgRDjny45'
+
+
+  useEffect(()=>{
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(response => {
+        const { data } = response
+        console.log(data)
+        const gifs = data.map(image => image.images.downsized_medium.url)
+        console.log(gifs)
+        setGifs(gifs)
+      })
+  }, [])
+
   return (
     <>
       <h1> Giphy App </h1>
+      {
+        gifs.map((singleGif)=>
+          <img src={singleGif} ></img>
+        )
+      }
+        
     </>
   )
 }
