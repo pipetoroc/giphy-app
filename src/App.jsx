40036fc3d657e60c1281233be9825771801757cf
Felipe Toro C <ftorocastilla@gmail.com>
2023-04-30 22:20:55 -0300

    refactor: create directory services and abstract the function get gifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import getGifs from './services/getGifs'
 
 function App () {
 
   const [gifs, setGifs] = useState([])
 
-  const  API_URL = 'https://api.giphy.com/v1/gifs/trending?api_key=pJqYviFUDNfFcku0l5QR8kqFgRDjny45&limit=25&rating=g'
-  const API_KEY = 'pJqYviFUDNfFcku0l5QR8kqFgRDjny45'
-
-
   useEffect(()=>{
-    fetch(API_URL)
-      .then(res => res.json())
-      .then(response => {
-        const { data } = response
-        console.log(data)
-        const gifs = data.map(image => image.images.downsized_medium.url)
-        console.log(gifs)
-        setGifs(gifs)
-      })
-  }, [])
+    getGifs().then(gifs => setGifs(gifs))
+      }, [])
 
   return (
     <>
