3ef27c0f248e4a0d0feed9bb597e19292cff7299
Felipe Toro C <ftorocastilla@gmail.com>
2023-05-10 18:17:37 -0300

    feat: input to search giphies

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,25 @@
 import TrendingGifs from '../../components/TrendingGifs/TrendingGifs'
+import { useState } from 'react'
 
 function Home () {
+  const [keyword, setKeyword] = useState('')
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    // navegar
+    console.log(keyword)
+  }
+
+  const hadleChange = evt => {
+    setKeyword(evt.target.value)
+  }
+
   return (
     <>
 
       <h1 className='home__title'> The  Giphy App </h1>
-        <form className='home__form'>
-          <input className='home__input' type="text" placeholder='Busca tu gif'/>
+        <form className='home__form' onSubmit={handleSubmit}>
+          <input className='home__input' type="text" placeholder='Busca tu gif' value={keyword} onChange={hadleChange}/>
         </form>
         <TrendingGifs/>
 
